b3e9d1164698761ef9c2eaacbf11226e8d0c2553
Melancholia66 <dead.girl.2012@mail.ru>
2023-02-06 15:28:44 +0100

    first commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,54 @@
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiImage,
+  EuiPanel,
+  EuiProvider,
+  EuiSpacer,
+  EuiText,
+  EuiTextColor,
+} from "@elastic/eui";
+import React from "react";
+import logo from "../assets/logo.png";
+import animation from "../assets/animation.gif";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseAuth, firebaseDB, usersRef } from "../utils/firebaseConfig";
+
+function Login() {
+  
+  
+  return (
+    <EuiProvider colorMode="dark">
+      <EuiFlexGroup
+        justifyContent="center"
+        alignItems="center"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <EuiFlexItem grow={false}>
+          <EuiPanel paddingSize="xl">
+            <EuiFlexGroup justifyContent="center" alignItems="center">
+              <EuiFlexItem>
+                <EuiImage src={animation} alt="logo" />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiImage src={logo} alt="logo" size="230px" />
+                <EuiSpacer size="xs" />
+                <EuiText textAlign="center" grow={false}>
+                  <h3>
+                    <EuiTextColor>One Platform to</EuiTextColor>
+                    <EuiTextColor color="#0b5cff"> connect</EuiTextColor>
+                  </h3>
+                </EuiText>
+                <EuiSpacer size="l" />
+                <EuiButton fill >Login with Google</EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiProvider>
+  );
+}
+
+export default Login;
